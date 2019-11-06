2ce568c484a7e8f31991c46dd32cae3ef7afa240
Enrico <enrico.bison@spindoxlabs.it>
2019-11-06 18:25:43 +0100

    init ex5.2

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -16,6 +16,7 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		fmt.Printf("%s\n", url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -27,7 +28,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%s", string(b))
 	}
 }
 
